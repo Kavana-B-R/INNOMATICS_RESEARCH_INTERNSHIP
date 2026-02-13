c3bd5a56ba34d49d8c7bb267520a981741aa0211
Kavana-B-R <166349383+Kavana-B-R@users.noreply.github.com>
2026-02-13 18:08:02 +0530

    Add files via upload

---
+++ TASK_1 LOGIC BUILDING/MESSAGE LENGTH/Message length 1.py
@@ -0,0 +1,8 @@
+messages = ["Hi", "Welcome to the platform", "OK"]
+
+for message in messages:
+    length = len(message)
+    print(f"Message: '{message}' | Length: {length}")
+
+    if length > 10:
+        print("⚠️ This message is longer than 10 characters.")
