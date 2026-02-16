e3d50a4461a76414cb2362ae8af374b78b321697
Kavana-B-R <166349383+Kavana-B-R@users.noreply.github.com>
2026-02-16 14:31:20 +0530

    Add files via upload

---
+++ TASK_2 LOGIC_BUILDING/MAXIMUM AND MINIMUM VALUES/Maximum and minimum values.py
@@ -0,0 +1,13 @@
+numbers = [45, 22, 89, 10, 66]
+maximum = numbers[0]
+minimum = numbers[0]
+
+for num in numbers:
+    if num > maximum:
+        maximum = num
+    if num < minimum:
+        minimum = num
+
+print("List:", numbers)
+print("Max:", maximum)
+print("Min:", minimum)
