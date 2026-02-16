1df36e35fef8044f7db922eec1b7da2489200679
Kavana-B-R <166349383+Kavana-B-R@users.noreply.github.com>
2026-02-16 14:25:03 +0530

    Add files via upload

---
+++ TASK_2 LOGIC_BUILDING/KEY EXISTS IN DICTIONARY/Key exists in a Dictionary.py
@@ -0,0 +1,12 @@
+# Problem 9: Check if a Key Exists in a Dictionary
+
+employees = {
+    "Ravi": 75000,
+    "Anita": 68000
+}
+
+# Checking if key exists
+if "Ravi" in employees:
+    print("Employee exists")
+else:
+    print("Employee does not exist")
